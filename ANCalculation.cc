443b1ca0f2c37f98a1c3230f63985b03c9a59321
Seunghwan Lee <zxzfdsa@gmail.com>
2025-04-04 00:23:53 -0400

    some options are inserted for main calculation

--- ANCalculation.cc
+++ ANCalculation.cc
@@ -1,12 +1,28 @@
 // AN for RHICf particles calculation and analysis
 
-#include <iostream>
+#include "RHICfAnAnalysis.hh"
 
 using namespace std;
 
 int main(int argc, char** argv)
 {
+    RHICfAnAnalysis* anAnalysis = new RHICfAnAnalysis();
+    anAnalysis -> SetRunType("TS");
+
+    anAnalysis -> ForceCalculateMass();
+    // anAnalysis -> ForceCalculateBinning(); 
+
+    anAnalysis -> CalculatePi0();  
+    anAnalysis -> SetExecuteEventNum(100000);    
+    anAnalysis -> SetExecuteFileNum(70);    
+
+    anAnalysis -> Init();
+    anAnalysis -> Calculate();
+    anAnalysis -> Finish();
+
 
     cout << "Terminated." << endl;
     return 0;
 }
+
+
