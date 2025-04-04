572f41186914042e633681d08f2e5164b77fb23f
Seunghwan Lee <zxzfdsa@gmail.com>
2025-04-04 00:16:45 -0400

    add the new option container class

---
+++ source/Util/RHICfOptContainer.cc
@@ -0,0 +1,222 @@
+#include "RHICfOptContainer.hh"
+
+#include "TROOT.h"
+#include "TSystem.h"
+#include "TSystemFile.h"
+#include "TSystemDirectory.h"
+#include "TObjArray.h"
+#include "TObjString.h"
+
+RHICfOptContainer* RHICfOptContainer::mOptContainer = nullptr;
+RHICfOptContainer* RHICfOptContainer::GetOptContainer()
+{
+    if (mOptContainer != nullptr){return mOptContainer;}
+    return new RHICfOptContainer();
+}
+
+RHICfOptContainer::RHICfOptContainer() 
+{
+    mOptContainer = this;
+    InitOptions();
+    FindDirPath();
+}
+
+RHICfOptContainer::~RHICfOptContainer()
+{
+}
+
+int RHICfOptContainer::Init()
+{
+    int sumParticleOpt = int(mGammaCalc)+int(mPi0Calc)+int(mNeutronCalc)+int(mLambda0Calc);
+    if(sumParticleOpt != 1){
+        cout << "RHICfOptContainer::Init() -- Error: set to only one particle calculation option!" << endl;
+        return 0;
+    }
+
+    return 1;
+}
+
+void RHICfOptContainer::SetRunType(TString type)
+{
+    type.ToUpper();
+    if(type.Index("TS") != -1){mRunType = kTSRun;}
+    else if(type.Index("TL") != -1){mRunType = kTLRun;}
+    else if(type.Index("TOP") != -1){mRunType = kTOPRun;}
+    else{mRunType = kALLRun;}
+}
+
+void RHICfOptContainer::SetInputDataPath(TString path){mInputDataPath = path;}
+void RHICfOptContainer::SetInputDataList(TString listFile){mInputDataList = listFile;}
+void RHICfOptContainer::SetExecuteEventNum(int event){mExecuteEventNum = event;}
+void RHICfOptContainer::SetExecuteFileNum(int num){mExecuteFileNum = num;}
+
+void RHICfOptContainer::ForceCalculateMass(){mForceCalculateMass = true;}
+void RHICfOptContainer::ForceCalculateBinning(){mForceCalculateBinning = true;}
+
+void RHICfOptContainer::CalculateGamma(){mGammaCalc = true;}
+void RHICfOptContainer::CalculatePi0(){mPi0Calc = true;}
+void RHICfOptContainer::CalculateNeutron(){mNeutronCalc = true;}
+void RHICfOptContainer::CalculateLambda0(){mLambda0Calc = true;}
+
+void RHICfOptContainer::SetOffDetPoint(){mOffDetPoint = true;}
+void RHICfOptContainer::SetOffParticle(){mOffParticle = true;}
+void RHICfOptContainer::SetOffTPCTrack(){mOffTPCTrack = true;}
+void RHICfOptContainer::SetOffBTof(){mOffBTof = true;}
+void RHICfOptContainer::SetOffBBC(){mOffBBC = true;}
+void RHICfOptContainer::SetOffVPD(){mOffVPD = true;}
+void RHICfOptContainer::SetOffZDC(){mOffZDC = true;}
+void RHICfOptContainer::SetOffFMS(){mOffFMS = true;}
+void RHICfOptContainer::SetOffRPS(){mOffRPS = true;}
+
+int RHICfOptContainer::GetRunType(){return mRunType;}
+
+int RHICfOptContainer::GetFillNumIdx(int fillNum)
+{
+    if(fillNum == 21142){return 0;}
+    if(fillNum == 21145){return 1;}
+    if(fillNum == 21148){return 2;}
+    if(fillNum == 21149){return 3;}
+    if(fillNum == 21150){return 4;}
+    return -1;
+}
+
+int RHICfOptContainer::GetFillNumToRunIdx(int fillNum)
+{
+    if(fillNum == 21142 || fillNum == 21145){return kTLRun;}
+    if(fillNum == 21148 || fillNum == 21150){return kTSRun;}
+    if(fillNum == 21149){return kTOPRun;}
+    return kALLRun;
+}
+
+int RHICfOptContainer::GetFillToRunIdx(int fillIdx)
+{
+    if(fillIdx == 0 || fillIdx == 1){return kTLRun;}
+    if(fillIdx == 2 || fillIdx == 4){return kTSRun;}
+    if(fillIdx == 3){return kTOPRun;}
+    return kALLRun;
+}
+
+
+TString RHICfOptContainer::GetRunTypeName(int runIdx)
+{
+    if(runIdx == kTLRun){return "TL";}
+    if(runIdx == kTSRun){return "TS";}
+    if(runIdx == kTOPRun){return "TOP";}
+    return "ALL";
+}
+
+TString RHICfOptContainer::GetDLEName(int dleIdx)
+{
+    if(dleIdx == kSDLE){return "SDLE";}
+    if(dleIdx == kDDLE){return "DDLE";}
+    if(dleIdx == kNDLE){return "NDLE";}
+    return "ALL";
+}
+
+TString RHICfOptContainer::GetInputDataPath(){return mInputDataPath;}
+TString RHICfOptContainer::GetInputDataList(){return mInputDataList;}
+TString RHICfOptContainer::GetTablePath(){return mTablePath;}
+TString RHICfOptContainer::GetDataPath(){return mDataPath;}
+TString RHICfOptContainer::GetFigurePath(){return mFigurePath;}
+int RHICfOptContainer::GetExecuteEventNum(){return mExecuteEventNum;}
+int RHICfOptContainer::GetExecuteFileNum(){return mExecuteFileNum;}
+
+bool RHICfOptContainer::IsForceCalculateMass(){return mForceCalculateMass;}
+bool RHICfOptContainer::IsForceCalculateBinning(){return mForceCalculateBinning;}
+
+bool RHICfOptContainer::GetOffDetPoint(){return mOffDetPoint;}
+bool RHICfOptContainer::GetOffParticle(){return mOffParticle;}
+bool RHICfOptContainer::GetOffTPCTrack(){return mOffTPCTrack;}
+bool RHICfOptContainer::GetOffBTof(){return mOffBTof;}
+bool RHICfOptContainer::GetOffBBC(){return mOffBBC;}
+bool RHICfOptContainer::GetOffVPD(){return mOffVPD;}
+bool RHICfOptContainer::GetOffZDC(){return mOffZDC;}
+bool RHICfOptContainer::GetOffFMS(){return mOffFMS;}
+bool RHICfOptContainer::GetOffRPS(){return mOffRPS;}
+
+TString RHICfOptContainer::GetParticleRunName()
+{
+    if(mGammaCalc){return "Gamma";}
+    if(mPi0Calc){return "Pi0";}
+    if(mNeutronCalc){return "Neutron";}
+    if(mLambda0Calc){return "Lambda0";}
+    return "Non";
+}
+
+Int_t RHICfOptContainer::GetParticleRunIdx()
+{
+    if(mGammaCalc){return kGammaRun;}
+    if(mPi0Calc){return kPi0Run;}
+    if(mNeutronCalc){return kNeutronRun;}
+    if(mLambda0Calc){return kLambda0Run;}
+    return -1;
+}
+
+void RHICfOptContainer::InitOptions()
+{
+    mRunType = kALLRun;
+    mInputDataPath = "/gpfs01/star/pwg/slee5/RHICfEvent"; // Default input path
+    mInputDataList = "";
+    mTablePath = "";
+    mDataPath = "";
+    mFigurePath = "";
+    mExecuteEventNum = -1;
+    mExecuteFileNum = -1;
+
+    mGammaCalc = false;
+    mPi0Calc = false;
+    mNeutronCalc = false;
+    mLambda0Calc = false;
+
+    mForceCalculateMass = false;
+    mForceCalculateBinning = false;
+
+    mOffDetPoint = false;
+    mOffParticle = false;
+    mOffTPCTrack = false;
+    mOffBTof = false;
+    mOffBBC = false;
+    mOffVPD = false;
+    mOffZDC = false;
+    mOffFMS = false;
+    mOffRPS = false;
+}
+
+void RHICfOptContainer::FindDirPath()
+{
+    // Find a directory
+    TString currentPath = gSystem -> pwd();
+    TObjArray *tokens = currentPath.Tokenize("/");
+    TString directory = "";
+
+    TList *listOfDirs;
+    TObject *objDir;
+    for(int i=0; i<tokens->GetEntries()-3; i++){
+        TString dirPath = "";
+        for(int j=0; j<tokens->GetEntries()-i; j++){
+            dirPath = dirPath+"/"+ ((TObjString *) tokens -> At(j)) -> GetString();
+        }
+        TSystemDirectory dir("dir", dirPath);
+        listOfDirs = dir.GetListOfFiles();
+        TIter next(listOfDirs);
+
+        while((objDir = next())){
+            TSystemFile* dirPtr = dynamic_cast<TSystemFile*>(objDir);
+            if(dirPtr && dirPtr->IsDirectory()){
+                TString dirName = dirPtr->GetName();
+                if(dirName.Index("tables") != -1){
+                    mTablePath = dirPath+"/tables";
+                    cout << "RHICfOptContainer::FindDirPath() -- Tables dir was found. " << mTablePath << endl;
+                }
+                if(dirName.Index("data") != -1){
+                    mDataPath = dirPath+"/data";
+                    cout << "RHICfOptContainer::FindDirPath() -- Data dir was found. " << mDataPath << endl;
+                }
+                if(dirName.Index("figure") != -1){
+                    mFigurePath = dirPath+"/figure";
+                    cout << "RHICfOptContainer::FindDirPath() -- Figure dir was found. " << mFigurePath << endl;
+                }
+            }
+        }
+    }
+}
---
+++ source/Util/RHICfOptContainer.hh
@@ -0,0 +1,145 @@
+#ifndef RHICfOptContainer_hh
+#define RHICfOptContainer_hh
+
+#include <iostream>
+#include "TString.h"
+
+enum parameters{
+    kTLRun = 0,
+    kTSRun = 1,
+    kTOPRun = 2,
+    kALLRun = -1,
+
+    kPhoton = 0,
+    kHadron = 1,
+
+    kSDLE = 0,
+    kDDLE = 1,
+    kNDLE = 2,
+    kALLDLE = 3,
+
+    kGammaRun = 0,
+    kPi0Run = 1,
+    kNeutronRun = 2,
+    kLambda0Run = 3,
+
+    kType1 = 0,
+    kType2 = 1,
+
+    kNULL = 0
+};
+
+static const double kRHICfPosZ = 17800.; // [mm]
+static const int kFillNum = 5;
+static const int kRunNum = 3; 
+static const int kTypeNum = 2;
+static const int kDLENum = 4;
+
+using namespace std;
+
+class RHICfOptContainer
+{
+    public:
+        static RHICfOptContainer* GetOptContainer();
+
+        RHICfOptContainer();
+        ~RHICfOptContainer();
+
+        virtual int Init();
+
+        // ========== Set function for options ==========
+        void SetRunType(TString type);
+        void SetInputDataPath(TString path);
+        void SetInputDataList(TString listFile);
+        void SetExecuteEventNum(int event);
+        void SetExecuteFileNum(int num);
+
+        void ForceCalculateMass();
+        void ForceCalculateBinning();
+
+        // specific particle calculation option
+        void CalculateGamma();
+        void CalculatePi0();
+        void CalculateNeutron();
+        void CalculateLambda0();
+
+        // ========== On-Off data options for RHICfEventDst
+        void SetOffDetPoint();
+        void SetOffParticle();
+        void SetOffTPCTrack();
+        void SetOffBTof();
+        void SetOffBBC();
+        void SetOffVPD();
+        void SetOffZDC();
+        void SetOffFMS();
+        void SetOffRPS();
+
+        // ========== Get function for options ==========
+        int GetRunType();
+        int GetFillNumIdx(int fillNum);
+        int GetFillNumToRunIdx(int fillNum);
+        int GetFillToRunIdx(int fillIdx);
+
+        TString GetRunTypeName(int runIdx=-1);
+        TString GetDLEName(int dleIdx);
+
+        TString GetInputDataPath();
+        TString GetInputDataList();
+        TString GetTablePath();
+        TString GetDataPath();
+        TString GetFigurePath();
+        int GetExecuteEventNum();
+        int GetExecuteFileNum();
+
+        bool IsForceCalculateMass();
+        bool IsForceCalculateBinning();
+
+        bool GetOffDetPoint();
+        bool GetOffParticle();
+        bool GetOffTPCTrack();
+        bool GetOffBTof();
+        bool GetOffBBC();
+        bool GetOffVPD();
+        bool GetOffZDC();
+        bool GetOffFMS();
+        bool GetOffRPS();
+
+        TString GetParticleRunName();
+        Int_t GetParticleRunIdx();
+
+    private:
+        void InitOptions();
+        void FindDirPath();
+
+        static RHICfOptContainer* mOptContainer;
+        
+        int mRunType;
+        TString mInputDataPath;
+        TString mInputDataList;
+        TString mTablePath;
+        TString mDataPath;
+        TString mFigurePath;
+        int mExecuteEventNum;
+        int mExecuteFileNum;
+
+        bool mGammaCalc;
+        bool mPi0Calc;
+        bool mNeutronCalc;
+        bool mLambda0Calc;
+
+        bool mForceCalculateMass;
+        bool mForceCalculateBinning;
+
+        bool mOffDetPoint;
+        bool mOffParticle;
+        bool mOffTPCTrack;
+        bool mOffBTof;
+        bool mOffBBC;
+        bool mOffVPD;
+        bool mOffZDC;
+        bool mOffFMS;
+        bool mOffRPS;
+
+};
+
+#endif
